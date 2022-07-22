3504138e22e4f14075fd723b41698b9f9c7768fa
Enes Baytemir <enesbytmr@gmail.com>
2022-07-23 01:29:04 +0300

    rmarray done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = (arr, ...manyArgs) => {
+  let result = [];
+ result = arr.filter(item => !manyArgs.includes(item))
+    return result
 };
+
+removeFromArray([1, 2, 3, 4, 5, 6], 3,2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
