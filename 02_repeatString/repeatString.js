ea16f7c2cc5a4b37b657a96deb1b400f4dbca9c4
Enes Baytemir <enesbytmr@gmail.com>
2022-07-23 00:27:02 +0300

    passed removeFromArray test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function(text,count) {
+    let result = "";
+    if(count < 0){
+         let err ="ERROR"
+         return err
+    }else
+    {
+    for (let i = 0; i < count; i++) {
+        result += text
+        }
+        return result;
+    }
+
 
 };
 
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    return(str.split("").reverse().join(""))
+
 
 };
+
+reverseString("deneme")
 
 // Do not edit below this line
 module.exports = reverseString;
