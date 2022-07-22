577b1c21e2b5ffb2a617034949090a91bcb73629
Enes Baytemir <enesbytmr@gmail.com>
2022-07-23 01:29:08 +0300

    sumall done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+   let sum = 0;
+    if(a>0 && b>0 &&  typeof a == "number" &&  typeof b == "number" ){
+        if(a<b){
+            for(a;a<=b;a++){
+                sum += a
+            }
+        }else{
+            for(b;b<=a;b++){
+                sum += b;
+            }
+        }
+        
+    }else{
+        return "ERROR"
+    }
+return sum;
 };
 
 // Do not edit below this line
