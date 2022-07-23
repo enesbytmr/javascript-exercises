b7d47d7f78f075c2737dae8145db48c3669f779c
Enes Baytemir <enesbytmr@gmail.com>
2022-07-23 13:01:18 +0300

    leap years solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
+const ftoc = function(index) {
 
+ let result = (index - 32) * (5/9)
+  result = result.toFixed(1)
+  result = Number(result);
+  return result
 };
 
-const ctof = function() {
-
+const ctof = function(index) {
+  let result = ((index * 9 ) / 5 ) + 32
+  result = result.toFixed(1)
+  result = Number(result);
+  return result
 };
 
 // Do not edit below this line
